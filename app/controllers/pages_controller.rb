6afd5bddd57c1460572725a61a87c3d3cd8278fb
Wallerand Dabas <wallerand.dabas.wd@gmail.com>
2016-05-09 21:09:39 +0000

    rails new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+  def team
+    @members = ["Got", "Simon", "Nico", "the beast"]
+  end
+  def join_us
+    render plain: "send a mail"
+  end
 end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+    def index
+      @products = Product.all
+    end
+    def show
+      @product = Product.find(params[:id])
+    end
+    def new
+      @product = Product.new
+    end
+    def create
+    end
+    def edit
+    end
+    def update
+    end
+    def destroy
+    end
+end
